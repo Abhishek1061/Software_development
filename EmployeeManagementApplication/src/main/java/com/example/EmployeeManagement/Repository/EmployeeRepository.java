f33bbd9e6a35f3f365d527a9b42861654641ea9f
Abhishek Kadam <abhishekkadam1061@gmail.com>
2026-09-22 00:49:48 +0530

    Remove unnecessary blank line in EmployeeRepository

--- EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Repository/EmployeeRepository.java
+++ EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.example.EmployeeManagement.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.EmployeeManagement.Entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
