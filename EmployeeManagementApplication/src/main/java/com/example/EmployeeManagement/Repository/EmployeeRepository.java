262d8b93f18d3403e1105ae451c8c8bf4e6bbe9d
Abhishek Kadam <abhishekkadam1061@gmail.com>
2026-09-22 00:46:45 +0530

    Add initial project structure with Maven setup and Spring Boot application

---
+++ EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.EmployeeManagement.Entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    
+    // Custom query methods can be defined here if needed
+}
+
---
+++ EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Service/EmployeeService.java
@@ -0,0 +1,6 @@
+package com.example.EmployeeManagement.Service;
+
+public class EmployeeService {    
+      
+    
+}
