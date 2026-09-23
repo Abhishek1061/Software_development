03f8c04472e376217f979f2886bf92434c85ef6d
Abhishek Kadam <abhishekkadam1061@gmail.com>
2026-09-23 23:57:21 +0530

    Fix import statements and correct @Autowired annotation in EmployeeService

--- EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Service/EmployeeService.java
+++ EmployeeManagementApplication/src/main/java/com/example/EmployeeManagement/Service/EmployeeService.java
@@ -1,9 +1,17 @@
 package com.example.EmployeeManagement.Service;
 
-@Service
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.EmployeeManagement.Entity.Employee;
+import com.example.EmployeeManagement.Repository.EmployeeRepository;
+
+@Service 
 public class EmployeeService {    
       
-    @Autowire
+    @Autowired 
     private EmployeeRepository employeeRepository;
 
     public List<Employee> getAllEmployees() {
